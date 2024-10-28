03f0f8d401c0cc37d62f15b95d5de2552772d590
m1rstan1978 <m1rstan@mail.ru>
2024-10-28 14:40:05 +0300

    add get request

---
+++ server/api/request/data.get.js
@@ -0,0 +1,58 @@
+import { Request } from "@/server/db/association/userRequest";
+import { Op, fn } from "sequelize";
+
+export default defineEventHandler(async event => {
+  const { page: pageQuery, pageSize: pageSizeQuery } = getQuery(event);
+  const querySearch = [
+    {
+      name: "search",
+      key: "textarea",
+    },
+    {
+      name: "house",
+      key: "house",
+    },
+  ];
+
+  const querySearchObj = {};
+  Object.entries(getQuery(event)).forEach(([key, value]) => {
+    const findSearchQuery = querySearch.find(el => el.name === key);
+    if (!!findSearchQuery) {
+      querySearchObj[findSearchQuery.key] = {
+        [Op.iLike]: fn("LOWER", `${value.toLowerCase()}%`),
+      };
+    }
+  });
+
+  const page = parseInt(pageQuery) || 1;
+  const pageSize = parseInt(pageSizeQuery) || 10;
+
+  try {
+    const foundRequests = await Request.findAll({
+      where: querySearchObj,
+      limit: pageSize,
+      offset: (page - 1) * pageSize,
+      raw: true,
+    });
+
+    const totalCount = await Request.count({
+      where: querySearchObj,
+    });
+
+    const startIndex = (page - 1) * pageSize + 1;
+    const endIndex = Math.min(startIndex + pageSize - 1, totalCount);
+
+    const recordInfo = `${startIndex}–${endIndex} из ${totalCount} записей`;
+
+    return {
+      totalPages: Math.ceil(totalCount / pageSize),
+      requests: foundRequests,
+      recordInfo,
+    };
+  } catch {
+    return createError({
+      statusCode: 500,
+      message: "Ошибка сервера",
+    });
+  }
+});
