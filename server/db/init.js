372938ae53e4e6f494605b5b5abed8feabf6aa3d
m1rstan1978 <m1rstan@mail.ru>
2024-10-31 09:46:38 +0300

    commit

--- server/db/init.js
+++ server/db/init.js
@@ -1,19 +1,3 @@
-// import { Sequelize } from "sequelize";
-
-// const database = process.env.DB_POSTGRESSQL_DATABASE;
-// const username = process.env.DB_POSTGRESSQL_USERNAME;
-// const password = process.env.DB_POSTGRESSQL_PASSWORD;
-// const host = process.env.DB_POSTGRESSQL_HOST;
-
-// const sequelize = new Sequelize(database, username, password, {
-//   host: host,
-//   dialect: "postgres",
-// });
-
-// sequelize.sync({ force: true });
-
-// export default sequelize;
-
 import pg from "pg";
 import { Sequelize } from "sequelize";
 
@@ -24,6 +8,6 @@ const sequelize = new Sequelize(process.env.DB_POSTGRES_LINK, {
   dialectModule: pg,
 });
 
-// sequelize.sync({ force: true });
+sequelize.sync({ alter: true });
 
 export default sequelize;
